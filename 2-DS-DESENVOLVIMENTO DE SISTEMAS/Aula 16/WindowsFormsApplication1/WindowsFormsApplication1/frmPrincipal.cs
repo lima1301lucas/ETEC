b55967137d2dd7134615b195c2da9cd2926e4662
Lucas Lima <56768879+lima1301lucas@users.noreply.github.com>
2021-10-21 19:58:40 -0300

    Add files via upload

---
+++ 2-DS-DESENVOLVIMENTO DE SISTEMAS/Aula 16/WindowsFormsApplication1/WindowsFormsApplication1/frmPrincipal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new frmSecundario().Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            new frmTerciario().Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            new frmQuartairo().Show();
+        }
+    }
+}
