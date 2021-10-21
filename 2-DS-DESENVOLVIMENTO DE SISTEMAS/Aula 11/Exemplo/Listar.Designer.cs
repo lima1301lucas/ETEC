7d3aa377774f775e0b95fa8d30e8d11f330b7c7f
Lucas Lima <56768879+lima1301lucas@users.noreply.github.com>
2021-10-21 19:52:29 -0300

    Add files via upload

---
+++ 2-DS-DESENVOLVIMENTO DE SISTEMAS/Aula 11/Exemplo/Listar.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace exemplo
+{
+    partial class Listar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtpesquisa = new System.Windows.Forms.TextBox();
+            this.btnpesquisar = new System.Windows.Forms.Button();
+            this.lista = new System.Windows.Forms.DataGridView();
+            this.btniserir = new System.Windows.Forms.Button();
+            this.btnsair = new System.Windows.Forms.Button();
+            this.txtnumero = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.lista)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtpesquisa
+            // 
+            this.txtpesquisa.Location = new System.Drawing.Point(21, 15);
+            this.txtpesquisa.Name = "txtpesquisa";
+            this.txtpesquisa.Size = new System.Drawing.Size(271, 20);
+            this.txtpesquisa.TabIndex = 0;
+            // 
+            // btnpesquisar
+            // 
+            this.btnpesquisar.Location = new System.Drawing.Point(403, 12);
+            this.btnpesquisar.Name = "btnpesquisar";
+            this.btnpesquisar.Size = new System.Drawing.Size(75, 23);
+            this.btnpesquisar.TabIndex = 1;
+            this.btnpesquisar.Text = "Pesquisar";
+            this.btnpesquisar.UseVisualStyleBackColor = true;
+            this.btnpesquisar.Click += new System.EventHandler(this.btnpesquisar_Click);
+            // 
+            // lista
+            // 
+            this.lista.AllowUserToAddRows = false;
+            this.lista.AllowUserToDeleteRows = false;
+            this.lista.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.lista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.lista.Location = new System.Drawing.Point(21, 51);
+            this.lista.Name = "lista";
+            this.lista.ReadOnly = true;
+            this.lista.Size = new System.Drawing.Size(457, 444);
+            this.lista.TabIndex = 2;
+            // 
+            // btniserir
+            // 
+            this.btniserir.Location = new System.Drawing.Point(21, 513);
+            this.btniserir.Name = "btniserir";
+            this.btniserir.Size = new System.Drawing.Size(206, 23);
+            this.btniserir.TabIndex = 3;
+            this.btniserir.Text = "Inserir";
+            this.btniserir.UseVisualStyleBackColor = true;
+            this.btniserir.Click += new System.EventHandler(this.btniserir_Click);
+            // 
+            // btnsair
+            // 
+            this.btnsair.Location = new System.Drawing.Point(282, 513);
+            this.btnsair.Name = "btnsair";
+            this.btnsair.Size = new System.Drawing.Size(196, 23);
+            this.btnsair.TabIndex = 4;
+            this.btnsair.Text = "Sair";
+            this.btnsair.UseVisualStyleBackColor = true;
+            this.btnsair.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // txtnumero
+            // 
+            this.txtnumero.Location = new System.Drawing.Point(303, 15);
+            this.txtnumero.Name = "txtnumero";
+            this.txtnumero.Size = new System.Drawing.Size(86, 20);
+            this.txtnumero.TabIndex = 5;
+            // 
+            // Listar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 548);
+            this.Controls.Add(this.txtnumero);
+            this.Controls.Add(this.btnsair);
+            this.Controls.Add(this.btniserir);
+            this.Controls.Add(this.lista);
+            this.Controls.Add(this.btnpesquisar);
+            this.Controls.Add(this.txtpesquisa);
+            this.Name = "Listar";
+            this.Text = "Listar";
+            this.Load += new System.EventHandler(this.Listar_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.lista)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtpesquisa;
+        private System.Windows.Forms.Button btnpesquisar;
+        private System.Windows.Forms.DataGridView lista;
+        private System.Windows.Forms.Button btniserir;
+        private System.Windows.Forms.Button btnsair;
+        private System.Windows.Forms.TextBox txtnumero;
+    }
+}
---
+++ 2-DS-DESENVOLVIMENTO DE SISTEMAS/Aula 11/Exemplo/Listar.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace exemplo
+{
+    public partial class Listar : Form
+    {
+        public Listar()
+        {
+            InitializeComponent();
+        }
+
+        public void Pesquisa(string valor)
+        {
+            //executar ações no banco
+            MySqlDataAdapter tabela = new MySqlDataAdapter("select RUANOME RUA , lclnome BAIRRO from XRUAS left join LOCAIS on lclcodigo=RUALOCAL where RUANOME like'%" + valor + "%'", Form1.conexao);
+            //left join junção que usa no SQL e relaciona mais de uma tabela
+            //funçao que quer executar, aonde quer executar
+
+            //associa com o objeto no VS
+            DataTable dados = new DataTable();
+
+            //Relaciona os dois obj
+            tabela.Fill(dados);
+
+            //Pegando os dados e colocando no datagrid
+            lista.DataSource = dados;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Listar_Load(object sender, EventArgs e)
+        {
+            Pesquisa(" ");
+        }
+
+        private void btniserir_Click(object sender, EventArgs e)
+        {
+            MySqlCommand comando = new MySqlCommand();
+            comando.Connection = Form1.conexao;
+            String ruanome = txtpesquisa.Text;
+            String rualocal = txtnumero.Text ;
+            comando.CommandText = "Insert into XRUAS (RUANOME, RUALOCAL) values ('" + ruanome + "' , '" + rualocal + "')";
+            comando.ExecuteNonQuery();
+            MessageBox.Show("Dados inseridos !");
+        }
+
+        private void btnpesquisar_Click(object sender, EventArgs e)
+        {
+            Pesquisa(txtpesquisa.Text);
+            
+        }
+    }
+}
