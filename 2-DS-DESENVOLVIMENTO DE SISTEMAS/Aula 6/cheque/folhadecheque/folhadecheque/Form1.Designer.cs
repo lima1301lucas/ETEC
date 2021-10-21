40873a84fef77e7e74b8fa44fe5e26fd9281255c
Lucas Lima <56768879+lima1301lucas@users.noreply.github.com>
2021-10-21 19:45:06 -0300

    Add files via upload

---
+++ 2-DS-DESENVOLVIMENTO DE SISTEMAS/Aula 6/cheque/folhadecheque/folhadecheque/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace folhadecheque
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numeroescrito = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(737, 107);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // numeroescrito
+            // 
+            this.numeroescrito.AutoSize = true;
+            this.numeroescrito.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numeroescrito.Location = new System.Drawing.Point(39, 176);
+            this.numeroescrito.Name = "numeroescrito";
+            this.numeroescrito.Size = new System.Drawing.Size(30, 24);
+            this.numeroescrito.TabIndex = 1;
+            this.numeroescrito.Text = "....";
+            this.numeroescrito.Click += new System.EventHandler(this.numeroescrito_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(731, 378);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(97, 47);
+            this.btnLimpar.TabIndex = 2;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::folhadecheque.Properties.Resources.cheque1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(840, 562);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.numeroescrito);
+            this.Controls.Add(this.textBox1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label numeroescrito;
+        private System.Windows.Forms.Button btnLimpar;
+
+    }
+}
+
---
+++ 2-DS-DESENVOLVIMENTO DE SISTEMAS/Aula 6/cheque/folhadecheque/folhadecheque/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace folhadecheque
+{
+    public partial class Form1 : Form
+    {
+     
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void caixadenumero_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text != "")
+            {
+                numeroescrito.Text = EscreverExtenso(Convert.ToDecimal(textBox1.Text));
+            }
+        }
+
+        private void numeroescrito_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public static string EscreverExtenso(decimal valor)
+        {
+            if (valor <= 0 | valor >= 100000)
+                return "Valor não suportado pelo sistema.";
+            else
+            {
+                string strValor = valor.ToString("000000000000000.00");
+                string valor_por_extenso = string.Empty;
+                for (int i = 0; i <= 15; i += 3)
+                {
+                    valor_por_extenso += Escrever_Valor_Extenso(Convert.ToDecimal(strValor.Substring(i, 3)));
+                    if (i == 0 & valor_por_extenso != string.Empty)
+                    {
+
+                    }
+                    else if (i == 9 & valor_por_extenso != string.Empty)
+                        if (Convert.ToInt32(strValor.Substring(9, 3)) > 0)
+                            valor_por_extenso += " MIL" + ((Convert.ToDecimal(strValor.Substring(12, 3)) > 0) ? " E " : string.Empty);
+                    if (i == 12)
+                    {
+                        
+                            
+
+                                if (Convert.ToInt64(strValor.Substring(0, 15)) == 1)
+                                    valor_por_extenso += " REAL";
+                                else if (Convert.ToInt64(strValor.Substring(0, 15)) > 1)
+                                    valor_por_extenso += " REAIS";
+
+                    }
+
+                }
+                return valor_por_extenso;
+            }
+        }
+        static string Escrever_Valor_Extenso(decimal valor)
+        {
+            if (valor <= 0)
+                return string.Empty;
+            else
+            {
+                string montagem = string.Empty;
+                if (valor > 0 & valor < 1)
+                {
+                    valor *= 100;
+                }
+                string strValor = valor.ToString("000");
+                int a = Convert.ToInt32(strValor.Substring(0, 1));
+                int b = Convert.ToInt32(strValor.Substring(1, 1));
+                int c = Convert.ToInt32(strValor.Substring(2, 1));
+                if (a == 1) montagem += (b + c == 0) ? "CEM" : "CENTO";
+                else if (a == 2) montagem += "DUZENTOS";
+                else if (a == 3) montagem += "TREZENTOS";
+                else if (a == 4) montagem += "QUATROCENTOS";
+                else if (a == 5) montagem += "QUINHENTOS";
+                else if (a == 6) montagem += "SEISCENTOS";
+                else if (a == 7) montagem += "SETECENTOS";
+                else if (a == 8) montagem += "OITOCENTOS";
+                else if (a == 9) montagem += "NOVECENTOS";
+                if (b == 1)
+                {
+                    if (c == 0) montagem += ((a > 0) ? " E " : string.Empty) + "DEZ";
+                    else if (c == 1) montagem += ((a > 0) ? " E " : string.Empty) + "ONZE";
+                    else if (c == 2) montagem += ((a > 0) ? " E " : string.Empty) + "DOZE";
+                    else if (c == 3) montagem += ((a > 0) ? " E " : string.Empty) + "TREZE";
+                    else if (c == 4) montagem += ((a > 0) ? " E " : string.Empty) + "QUATORZE";
+                    else if (c == 5) montagem += ((a > 0) ? " E " : string.Empty) + "QUINZE";
+                    else if (c == 6) montagem += ((a > 0) ? " E " : string.Empty) + "DEZESSEIS";
+                    else if (c == 7) montagem += ((a > 0) ? " E " : string.Empty) + "DEZESSETE";
+                    else if (c == 8) montagem += ((a > 0) ? " E " : string.Empty) + "DEZOITO";
+                    else if (c == 9) montagem += ((a > 0) ? " E " : string.Empty) + "DEZENOVE";
+                }
+                else if (b == 2) montagem += ((a > 0) ? " E " : string.Empty) + "VINTE";
+                else if (b == 3) montagem += ((a > 0) ? " E " : string.Empty) + "TRINTA";
+                else if (b == 4) montagem += ((a > 0) ? " E " : string.Empty) + "QUARENTA";
+                else if (b == 5) montagem += ((a > 0) ? " E " : string.Empty) + "CINQUENTA";
+                else if (b == 6) montagem += ((a > 0) ? " E " : string.Empty) + "SESSENTA";
+                else if (b == 7) montagem += ((a > 0) ? " E " : string.Empty) + "SETENTA";
+                else if (b == 8) montagem += ((a > 0) ? " E " : string.Empty) + "OITENTA";
+                else if (b == 9) montagem += ((a > 0) ? " E " : string.Empty) + "NOVENTA";
+                if (strValor.Substring(1, 1) != "1" & c != 0 & montagem != string.Empty) montagem += " E ";
+                if (strValor.Substring(1, 1) != "1")
+                    if (c == 1) montagem += "UM";
+                    else if (c == 2) montagem += "DOIS";
+                    else if (c == 3) montagem += "TRÊS";
+                    else if (c == 4) montagem += "QUATRO";
+                    else if (c == 5) montagem += "CINCO";
+                    else if (c == 6) montagem += "SEIS";
+                    else if (c == 7) montagem += "SETE";
+                    else if (c == 8) montagem += "OITO";
+                    else if (c == 9) montagem += "NOVE";
+                return montagem;
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            numeroescrito.Text = "";
+            textBox1.Text = "";
+        }
+    }
+}
