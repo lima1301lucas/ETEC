d6a364ecfcd89223824a8ab2f06e456d13b04603
Lucas Lima <56768879+lima1301lucas@users.noreply.github.com>
2021-10-21 19:49:19 -0300

    Add files via upload

---
+++ 2-DS-DESENVOLVIMENTO DE SISTEMAS/Aula 9/Exercicio/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Exercicio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txt_valor = new System.Windows.Forms.TextBox();
+            this.btn_avancar = new System.Windows.Forms.Button();
+            this.Pn_letra = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // txt_valor
+            // 
+            this.txt_valor.Location = new System.Drawing.Point(34, 12);
+            this.txt_valor.MaxLength = 2;
+            this.txt_valor.Name = "txt_valor";
+            this.txt_valor.Size = new System.Drawing.Size(893, 20);
+            this.txt_valor.TabIndex = 0;
+            this.txt_valor.TextChanged += new System.EventHandler(this.txt_valor_TextChanged);
+            // 
+            // btn_avancar
+            // 
+            this.btn_avancar.Location = new System.Drawing.Point(942, 12);
+            this.btn_avancar.Name = "btn_avancar";
+            this.btn_avancar.Size = new System.Drawing.Size(92, 23);
+            this.btn_avancar.TabIndex = 0;
+            this.btn_avancar.Text = "Transformar";
+            this.btn_avancar.UseVisualStyleBackColor = true;
+            this.btn_avancar.Click += new System.EventHandler(this.btn_avancar_Click);
+            // 
+            // Pn_letra
+            // 
+            this.Pn_letra.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.Pn_letra.Location = new System.Drawing.Point(34, 52);
+            this.Pn_letra.Name = "Pn_letra";
+            this.Pn_letra.Size = new System.Drawing.Size(1000, 500);
+            this.Pn_letra.TabIndex = 1;
+            this.Pn_letra.Paint += new System.Windows.Forms.PaintEventHandler(this.Pn_letra_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1059, 574);
+            this.Controls.Add(this.btn_avancar);
+            this.Controls.Add(this.Pn_letra);
+            this.Controls.Add(this.txt_valor);
+            this.Name = "Form1";
+            this.Text = "TRANFORMA E CORTA SUAS FRASES ;(";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_valor;
+        private System.Windows.Forms.Button btn_avancar;
+        private System.Windows.Forms.Panel Pn_letra;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ 2-DS-DESENVOLVIMENTO DE SISTEMAS/Aula 9/Exercicio/Form1.cs
@@ -0,0 +1,416 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exercicio
+{
+    public partial class Form1 : Form
+    {
+       
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        public Graphics grafico;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Pn_letra.BackgroundImage = new Bitmap(40 ,50);
+            grafico = Graphics.FromImage(Pn_letra.BackgroundImage);
+
+        }
+
+        
+
+        private byte[,] FnDesenho(string Letra)
+        {
+            int frase = txt_valor.Text.Length;
+
+
+            
+            byte[,] Valor = {
+                           {0,0,0,0,0},
+                           {0,0,0,0,0},
+                           {0,0,0,0,0},
+                           {0,0,0,0,0},
+                           {0,0,0,0,0},
+                           {0,0,0,0,0}
+                           };
+
+            if (Letra == "A")
+            {
+                Valor = new byte [,]
+                {
+                           {0,1,1,1,0},
+                           {1,0,0,0,1},
+                           {1,0,0,0,1},
+                           {1,1,1,1,1},
+                           {1,0,0,0,1},
+                           {1,0,0,0,1}
+                };
+            }
+
+            else if (Letra == "B")
+            {
+                Valor = new byte[,]
+                {
+                           {1,1,1,1,0},
+                           {1,0,0,0,1},
+                           {1,1,1,1,0},
+                           {1,0,0,0,1},
+                           {1,0,0,0,1},
+                           {1,1,1,1,1}
+                };
+            }
+
+            else if (Letra == "C")
+            {
+                Valor = new byte[,]
+                {
+                           {1,1,1,1,1},
+                           {1,0,0,0,0},
+                           {1,0,0,0,0},
+                           {1,0,0,0,0},
+                           {1,0,0,0,0},
+                           {1,1,1,1,1}
+                };
+            }
+
+            else if (Letra == "D")
+            {
+                Valor = new byte[,]
+                {
+                           {1,1,1,1,0},
+                           {1,0,0,0,1},
+                           {1,0,0,0,1},
+                           {1,0,0,0,1},
+                           {1,0,0,0,1},
+                           {1,1,1,1,0}
+                };
+            }
+
+            else if (Letra == "E")
+            {
+                Valor = new byte[,]
+                {
+                           {1,1,1,1,1},
+                           {1,0,0,0,0},
+                           {1,0,0,0,0},
+                           {1,1,1,1,0},
+                           {1,0,0,0,0},
+                           {1,1,1,1,1}
+                };
+            }
+
+            else if (Letra == "F")
+            {
+                Valor = new byte[,]
+                {
+                           {1,1,1,1,1},
+                           {1,0,0,0,0},
+                           {1,0,0,0,0},
+                           {1,1,1,1,0},
+                           {1,0,0,0,0},
+                           {1,0,0,0,0}
+                };
+            }
+
+            else if (Letra == "G")
+            {
+                Valor = new byte[,]
+                {
+                           {1,1,1,1,1},
+                           {1,0,0,0,0},
+                           {1,0,0,0,0},
+                           {1,0,1,1,1},
+                           {1,0,0,0,1},
+                           {1,1,1,1,0}
+                };
+            }
+
+            else if (Letra == "H")
+            {
+                Valor = new byte[,]
+                {
+                           {1,0,0,0,1},
+                           {1,0,0,0,1},
+                           {1,1,1,1,1},
+                           {1,0,0,0,1},
+                           {1,0,0,0,1},
+                           {1,0,0,0,1}
+                };
+            }
+
+            else if (Letra == "I")
+            {
+                Valor = new byte[,]
+                {
+                           {1,1,1,1,1},
+                           {0,0,1,0,0},
+                           {0,0,1,0,0},
+                           {0,0,1,0,0},
+                           {0,0,1,0,0},
+                           {1,1,1,1,1}
+                };
+            }
+
+            else if (Letra == "J")
+            {
+                Valor = new byte[,]
+                {
+                           {1,1,1,1,1},
+                           {0,0,0,1,0},
+                           {0,0,0,1,0},
+                           {1,0,0,1,0},
+                           {0,1,0,1,0},
+                           {0,0,1,1,0}
+                };
+            }
+
+            else if (Letra == "K")
+            {
+                Valor = new byte[,]
+                {
+                           {1,0,0,1,0},
+                           {1,0,1,0,0},
+                           {1,1,0,0,0},
+                           {1,1,0,0,0},
+                           {1,0,1,0,0},
+                           {1,0,0,1,0}
+                };
+            }
+
+            else if (Letra == "L")
+            {
+                Valor = new byte[,]
+                {
+                           {1,0,0,0,0},
+                           {1,0,0,0,0},
+                           {1,0,0,0,0},
+                           {1,0,0,0,0},
+                           {1,0,0,0,0},
+                           {1,1,1,1,0}
+                };
+            }
+
+            else if (Letra == "M")
+            {
+                Valor = new byte[,]
+                {
+                           {1,0,0,0,1},
+                           {1,1,0,1,1},
+                           {1,0,1,0,1},
+                           {1,0,0,0,1},
+                           {1,0,0,0,1},
+                           {1,0,0,0,1}
+                };
+            }
+
+            else if (Letra == "N")
+            {
+                Valor = new byte[,]
+                {
+                           {0,1,1,1,0},
+                           {1,0,0,0,1},
+                           {1,0,0,0,1},
+                           {1,0,0,0,1},
+                           {1,0,0,0,1},
+                           {1,0,0,0,1}
+                };
+            }
+
+            else if (Letra == "O")
+            {
+                Valor = new byte[,]
+                {
+                           {0,1,1,1,0},
+                           {1,0,0,0,1},
+                           {1,0,0,0,1},
+                           {1,0,0,0,1},
+                           {1,0,0,0,1},
+                           {0,1,1,1,0}
+                };
+            }
+
+            else if (Letra == "P")
+            {
+                Valor = new byte[,]
+                {
+                           {1,1,1,1,0},
+                           {1,0,0,0,1},
+                           {1,0,0,0,1},
+                           {1,1,1,1,0},
+                           {1,0,0,0,0},
+                           {1,0,0,0,0}
+                };
+            }
+
+            else if (Letra == "Q" || Letra == "q")
+            {
+                Valor = new byte[,]
+                {
+                           {0,1,1,1,0},
+                           {1,0,0,0,1},
+                           {1,0,0,0,1},
+                           {1,0,0,0,1},
+                           {1,0,0,1,0},
+                           {0,1,1,0,1}
+                };
+            }
+
+            else if (Letra == "R")
+            {
+                Valor = new byte[,]
+                {
+                           {1,1,1,1,0},
+                           {1,0,0,0,1},
+                           {1,1,1,1,0},
+                           {1,0,1,0,0},
+                           {1,0,0,1,0},
+                           {1,0,0,0,1}
+                };
+            }
+
+            else if (Letra == "S")
+            {
+                Valor = new byte[,]
+                {
+                           {1,1,1,1,1},
+                           {1,0,1,0,0},
+                           {1,1,1,1,1},
+                           {0,0,0,0,1},
+                           {0,0,0,0,1},
+                           {1,1,1,1,1}
+                };
+            }
+
+            else if (Letra == "T")
+            {
+                Valor = new byte[,]
+                {
+                           {1,1,1,1,1},
+                           {0,0,1,0,0},
+                           {0,0,1,0,0},
+                           {0,0,1,0,0},
+                           {0,0,1,0,0},
+                           {0,0,1,0,0}
+                };
+            }
+
+            else if (Letra == "U")
+            {
+                Valor = new byte[,]
+                {
+                           {1,0,0,0,1},
+                           {1,0,0,0,1},
+                           {1,0,0,0,1},
+                           {1,0,0,0,1},
+                           {1,0,0,0,1},
+                           {0,1,1,1,0}
+                };
+            }
+
+            else if (Letra == "V")
+            {
+                Valor = new byte[,]
+                {
+                           {1,0,0,0,1},
+                           {1,0,0,0,1},
+                           {1,0,0,0,1},
+                           {1,0,0,0,1},
+                           {0,1,0,1,0},
+                           {0,0,1,0,0}
+                };
+            }
+
+            else if (Letra == "W")
+            {
+                Valor = new byte[,]
+                {
+                           {1,0,0,0,1},
+                           {1,0,0,0,1},
+                           {1,0,0,0,1},
+                           {1,0,1,0,1},
+                           {1,1,0,1,1},
+                           {1,0,0,0,1}
+                };
+            }
+
+
+            else if (Letra == "X")
+            {
+                Valor = new byte[,]
+                {
+                           {1,0,0,0,1},
+                           {0,1,0,1,0},
+                           {0,0,1,0,0},
+                           {0,0,1,0,0},
+                           {0,1,0,1,0},
+                           {1,0,0,0,1}
+                };
+            }
+
+            else if (Letra == "Y")
+            {
+                Valor = new byte[,]
+                {
+                           {1,0,0,0,1},
+                           {1,0,0,0,1},
+                           {0,1,0,1,0},
+                           {0,0,1,0,0},
+                           {0,0,1,0,0},
+                           {0,0,1,0,0}
+                };
+            }
+
+            else if(Letra == "Z")
+            {
+                Valor = new byte[,]
+                {
+                           {1,1,1,1,1},
+                           {0,0,0,0,1},
+                           {0,0,0,1,0},
+                           {0,0,1,0,0},
+                           {0,1,0,0,0},
+                           {1,1,1,1,1}
+                };
+            }
+            
+        return Valor;
+        }
+
+        private void txt_valor_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_avancar_Click(object sender, EventArgs e)
+        {
+            grafico.Clear(Color.White);
+            byte[,] desenho = FnDesenho(txt_valor.Text);
+            for (int y = 0; y<6; y++)
+            {
+                for (int x = 0; x<5; x++)
+                {
+                    grafico.FillEllipse(new SolidBrush(Color.Black), (x * 5), (y * 5), (desenho[y, x]) * 5, ((desenho[y, x] * 5)));
+                }
+            }
+            Pn_letra.Refresh();
+        }
+
+        private void Pn_letra_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        
+    }
+}
