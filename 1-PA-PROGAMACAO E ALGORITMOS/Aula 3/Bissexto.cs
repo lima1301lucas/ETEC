db7860e5c63313ad37c0d588e11644c0924dc52a
Lucas Lima <56768879+lima1301lucas@users.noreply.github.com>
2021-10-21 18:42:43 -0300

    Add files via upload

---
+++ 1-PA-PROGAMACAO E ALGORITMOS/Aula 3/Bissexto.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bissexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaração de variáveis
+            int a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, b;
+
+            //Atribuição de valores
+            a1 = 2000;
+            a2 = 2001;
+            a3 = 2002;
+            a4 = 2003;
+            a5 = 2004;
+            a6 = 2005;
+            a7 = 2006;
+            a8 = 2007;
+            a9 = 2008;
+            a10 = 2009;
+            a11 = 2010;
+            a12 = 2011;
+            a13 = 2012;
+            a14 = 2013;
+            a15 = 2014;
+            a16 = 2015;
+            a17 = 2016;
+            a18 = 2017;
+            a19 = 2018;
+            a20 = 2019;
+            b = 4;
+
+            Console.WriteLine(a1+"%"+b+"="+(a1 % b));
+            Console.WriteLine(a2 + "%" + b + "=" + (a2 % b));
+            Console.WriteLine(a3 + "%" + b + "=" + (a3 % b));
+            Console.WriteLine(a4 + "%" + b + "=" + (a4 % b));
+            Console.WriteLine(a5 + "%" + b + "=" + (a5 % b));
+            Console.WriteLine(a6 + "%" + b + "=" + (a6 % b));
+            Console.WriteLine(a7 + "%" + b + "=" + (a7 % b));
+            Console.WriteLine(a8 + "%" + b + "=" + (a8 % b));
+            Console.WriteLine(a9 + "%" + b + "=" + (a9 % b));
+            Console.WriteLine(a10 + "%" + b + "=" + (a10 % b));
+            Console.WriteLine(a11 + "%" + b + "=" + (a11 % b));
+            Console.WriteLine(a12 + "%" + b + "=" + (a12 % b));
+            Console.WriteLine(a13 + "%" + b + "=" + (a13 % b));
+            Console.WriteLine(a14 + "%" + b + "=" + (a14 % b));
+            Console.WriteLine(a15 + "%" + b + "=" + (a15 % b));
+            Console.WriteLine(a16 + "%" + b + "=" + (a16 % b));
+            Console.WriteLine(a17 + "%" + b + "=" + (a17 % b));
+            Console.WriteLine(a18 + "%" + b + "=" + (a18 % b));
+            Console.WriteLine(a19 + "%" + b + "=" + (a19 % b));
+            Console.WriteLine(a20 + "%" + b + "=" + (a20 % b));
+            Console.WriteLine("\n\nAperte ESC para sair");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
