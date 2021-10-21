3ea179f9c3a8fa891399738fa3685b46bbb2d266
Lucas Lima <56768879+lima1301lucas@users.noreply.github.com>
2021-10-21 19:27:00 -0300

    Add files via upload

---
+++ 2-DS-DESENVOLVIMENTO DE SISTEMAS/Aula 2/exemplo 3/exemplo 3/Calculadora.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace exemplo_3
+{
+    partial class Calculadora
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ctxResult = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.btnMult = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.bntSoma = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btnMaisMenos = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnVirg = new System.Windows.Forms.Button();
+            this.btn_igual = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ctxResult
+            // 
+            this.ctxResult.Enabled = false;
+            this.ctxResult.Font = new System.Drawing.Font("Swis721 Hv BT", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ctxResult.Location = new System.Drawing.Point(11, 9);
+            this.ctxResult.Name = "ctxResult";
+            this.ctxResult.Size = new System.Drawing.Size(238, 36);
+            this.ctxResult.TabIndex = 18;
+            this.ctxResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.White;
+            this.btnClear.Font = new System.Drawing.Font("Swis721 Hv BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(10, 54);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(52, 44);
+            this.btnClear.TabIndex = 19;
+            this.btnClear.Text = "C";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.BackColor = System.Drawing.Color.White;
+            this.btnDiv.Font = new System.Drawing.Font("Swis721 Hv BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDiv.Location = new System.Drawing.Point(73, 54);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(52, 44);
+            this.btnDiv.TabIndex = 20;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = false;
+            this.btnDiv.Click += new System.EventHandler(this.operacao);
+            // 
+            // btnMult
+            // 
+            this.btnMult.BackColor = System.Drawing.Color.White;
+            this.btnMult.Font = new System.Drawing.Font("Swis721 Hv BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMult.Location = new System.Drawing.Point(136, 54);
+            this.btnMult.Name = "btnMult";
+            this.btnMult.Size = new System.Drawing.Size(52, 44);
+            this.btnMult.TabIndex = 21;
+            this.btnMult.Text = "*";
+            this.btnMult.UseVisualStyleBackColor = false;
+            this.btnMult.Click += new System.EventHandler(this.operacao);
+            // 
+            // btnSub
+            // 
+            this.btnSub.BackColor = System.Drawing.Color.White;
+            this.btnSub.Font = new System.Drawing.Font("Swis721 Hv BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSub.Location = new System.Drawing.Point(197, 54);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(52, 44);
+            this.btnSub.TabIndex = 22;
+            this.btnSub.Text = "-";
+            this.btnSub.UseVisualStyleBackColor = false;
+            this.btnSub.Click += new System.EventHandler(this.operacao);
+            // 
+            // bntSoma
+            // 
+            this.bntSoma.BackColor = System.Drawing.Color.White;
+            this.bntSoma.Font = new System.Drawing.Font("Swis721 Hv BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntSoma.Location = new System.Drawing.Point(197, 104);
+            this.bntSoma.Name = "bntSoma";
+            this.bntSoma.Size = new System.Drawing.Size(52, 44);
+            this.bntSoma.TabIndex = 26;
+            this.bntSoma.Text = "+";
+            this.bntSoma.UseVisualStyleBackColor = false;
+            this.bntSoma.Click += new System.EventHandler(this.operacao);
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.Color.White;
+            this.btn9.Font = new System.Drawing.Font("Swis721 Hv BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn9.Location = new System.Drawing.Point(136, 104);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(52, 44);
+            this.btn9.TabIndex = 25;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = false;
+            this.btn9.Click += new System.EventHandler(this.exibenumero);
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.Color.White;
+            this.btn8.Font = new System.Drawing.Font("Swis721 Hv BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn8.Location = new System.Drawing.Point(73, 104);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(52, 44);
+            this.btn8.TabIndex = 24;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = false;
+            this.btn8.Click += new System.EventHandler(this.exibenumero);
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.Color.White;
+            this.btn7.Font = new System.Drawing.Font("Swis721 Hv BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn7.Location = new System.Drawing.Point(10, 104);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(52, 44);
+            this.btn7.TabIndex = 23;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = false;
+            this.btn7.Click += new System.EventHandler(this.exibenumero);
+            // 
+            // btnMaisMenos
+            // 
+            this.btnMaisMenos.BackColor = System.Drawing.Color.White;
+            this.btnMaisMenos.Font = new System.Drawing.Font("Swis721 Hv BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMaisMenos.Location = new System.Drawing.Point(197, 154);
+            this.btnMaisMenos.Name = "btnMaisMenos";
+            this.btnMaisMenos.Size = new System.Drawing.Size(52, 44);
+            this.btnMaisMenos.TabIndex = 30;
+            this.btnMaisMenos.Text = "+/-";
+            this.btnMaisMenos.UseVisualStyleBackColor = false;
+            this.btnMaisMenos.Click += new System.EventHandler(this.btnMaisMenos_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.Color.White;
+            this.btn6.Font = new System.Drawing.Font("Swis721 Hv BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn6.Location = new System.Drawing.Point(136, 154);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(52, 44);
+            this.btn6.TabIndex = 29;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = false;
+            this.btn6.Click += new System.EventHandler(this.exibenumero);
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.Color.White;
+            this.btn5.Font = new System.Drawing.Font("Swis721 Hv BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn5.Location = new System.Drawing.Point(73, 154);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(52, 44);
+            this.btn5.TabIndex = 28;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = false;
+            this.btn5.Click += new System.EventHandler(this.exibenumero);
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.Color.White;
+            this.btn4.Font = new System.Drawing.Font("Swis721 Hv BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn4.Location = new System.Drawing.Point(10, 154);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(52, 44);
+            this.btn4.TabIndex = 27;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = false;
+            this.btn4.Click += new System.EventHandler(this.exibenumero);
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.Color.White;
+            this.btn3.Font = new System.Drawing.Font("Swis721 Hv BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn3.Location = new System.Drawing.Point(136, 204);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(52, 44);
+            this.btn3.TabIndex = 33;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = false;
+            this.btn3.Click += new System.EventHandler(this.exibenumero);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.White;
+            this.btn2.Font = new System.Drawing.Font("Swis721 Hv BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn2.Location = new System.Drawing.Point(73, 204);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(52, 44);
+            this.btn2.TabIndex = 32;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.exibenumero);
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.Color.White;
+            this.btn1.Font = new System.Drawing.Font("Swis721 Hv BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn1.Location = new System.Drawing.Point(10, 204);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(52, 44);
+            this.btn1.TabIndex = 31;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = false;
+            this.btn1.Click += new System.EventHandler(this.exibenumero);
+            // 
+            // btn0
+            // 
+            this.btn0.BackColor = System.Drawing.Color.White;
+            this.btn0.Font = new System.Drawing.Font("Swis721 Hv BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn0.Location = new System.Drawing.Point(11, 254);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(114, 44);
+            this.btn0.TabIndex = 34;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = false;
+            this.btn0.Click += new System.EventHandler(this.exibenumero);
+            // 
+            // btnVirg
+            // 
+            this.btnVirg.BackColor = System.Drawing.Color.White;
+            this.btnVirg.Font = new System.Drawing.Font("Swis721 Hv BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVirg.Location = new System.Drawing.Point(136, 254);
+            this.btnVirg.Name = "btnVirg";
+            this.btnVirg.Size = new System.Drawing.Size(52, 44);
+            this.btnVirg.TabIndex = 35;
+            this.btnVirg.Text = ",";
+            this.btnVirg.UseVisualStyleBackColor = false;
+            this.btnVirg.Click += new System.EventHandler(this.btnVirg_Click);
+            // 
+            // btn_igual
+            // 
+            this.btn_igual.BackColor = System.Drawing.Color.White;
+            this.btn_igual.Font = new System.Drawing.Font("Swis721 Hv BT", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_igual.Location = new System.Drawing.Point(197, 204);
+            this.btn_igual.Name = "btn_igual";
+            this.btn_igual.Size = new System.Drawing.Size(52, 94);
+            this.btn_igual.TabIndex = 36;
+            this.btn_igual.Text = "=";
+            this.btn_igual.UseVisualStyleBackColor = false;
+            this.btn_igual.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Calculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(261, 305);
+            this.Controls.Add(this.btn_igual);
+            this.Controls.Add(this.btnVirg);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btnMaisMenos);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.bntSoma);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btnSub);
+            this.Controls.Add(this.btnMult);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.ctxResult);
+            this.Name = "Calculadora";
+            this.Text = "Calculadora";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ctxResult;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Button btnMult;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button bntSoma;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btnMaisMenos;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnVirg;
+        private System.Windows.Forms.Button btn_igual;
+    }
+}
+
---
+++ 2-DS-DESENVOLVIMENTO DE SISTEMAS/Aula 2/exemplo 3/exemplo 3/Calculadora.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exemplo_3
+{
+    public partial class Calculadora : Form
+    {
+        Double n1;
+        Char ope;
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double n1 = 0, re = 0;
+            n1 = double.Parse(ctxResult.Text);
+
+            switch (ope)
+            {
+                case '+': ctxResult.Text = (this.n1 + n1).ToString(); break;
+                case '-': ctxResult.Text = (this.n1 - n1).ToString(); break;
+                case '*': ctxResult.Text = (this.n1 * n1).ToString(); break;
+                case '/': ctxResult.Text = (this.n1 / n1).ToString(); 
+                    if (n1 == 0)
+                    {
+                        MessageBox.Show("Impossível dividir por 0");
+
+                    }
+                    else
+                        re = this.n1 / n1;
+                    ctxResult.Text = re.ToString();
+                    break;
+
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exibenumero(object sender, EventArgs e)
+        {
+            ctxResult.Text += ((Button)sender).Text;
+        }
+
+        private void operacao(object sender, EventArgs e)
+        {
+            n1 = double.Parse(ctxResult.Text);
+            ctxResult.Clear();
+            ope = char.Parse(((Button)sender).Text);
+            
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ctxResult.Clear();
+        }
+
+        private void btnMaisMenos_Click(object sender, EventArgs e)
+        {
+            ctxResult.Text = (double.Parse(ctxResult.Text) * -1).ToString();
+        }
+
+        private void btnVirg_Click(object sender, EventArgs e)
+        {
+            if (ctxResult.Text.IndexOf(',') < 0)
+            {
+                ctxResult.Text += ',';
+            }
+        }
+
+    }
+}
