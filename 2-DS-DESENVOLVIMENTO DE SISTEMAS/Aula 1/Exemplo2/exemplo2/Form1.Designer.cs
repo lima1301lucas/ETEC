9327c5ab3372e20639fc1eab1a6e10af646efbb9
Lucas Lima <56768879+lima1301lucas@users.noreply.github.com>
2021-10-21 19:23:11 -0300

    Add files via upload

---
+++ 2-DS-DESENVOLVIMENTO DE SISTEMAS/Aula 1/Exemplo2/exemplo2/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace exemplo2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.vScrollBar2 = new System.Windows.Forms.VScrollBar();
+            this.vScrollBar3 = new System.Windows.Forms.VScrollBar();
+            this.SuspendLayout();
+            // 
+            // txt_1
+            // 
+            this.txt_1.Location = new System.Drawing.Point(12, 138);
+            this.txt_1.Name = "txt_1";
+            this.txt_1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txt_1.Size = new System.Drawing.Size(102, 20);
+            this.txt_1.TabIndex = 0;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(505, 138);
+            this.txt2.Name = "txt2";
+            this.txt2.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txt2.Size = new System.Drawing.Size(101, 20);
+            this.txt2.TabIndex = 1;
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(262, 138);
+            this.txt3.Name = "txt3";
+            this.txt3.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.txt3.Size = new System.Drawing.Size(99, 20);
+            this.txt3.TabIndex = 2;
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(149, 19);
+            this.vScrollBar1.Maximum = 255;
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(19, 289);
+            this.vScrollBar1.TabIndex = 3;
+            this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_Scroll);
+            // 
+            // vScrollBar2
+            // 
+            this.vScrollBar2.Location = new System.Drawing.Point(620, 9);
+            this.vScrollBar2.Maximum = 255;
+            this.vScrollBar2.Name = "vScrollBar2";
+            this.vScrollBar2.Size = new System.Drawing.Size(19, 299);
+            this.vScrollBar2.TabIndex = 4;
+            this.vScrollBar2.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar2_Scroll);
+            // 
+            // vScrollBar3
+            // 
+            this.vScrollBar3.Location = new System.Drawing.Point(374, 9);
+            this.vScrollBar3.Maximum = 255;
+            this.vScrollBar3.Name = "vScrollBar3";
+            this.vScrollBar3.Size = new System.Drawing.Size(19, 285);
+            this.vScrollBar3.TabIndex = 5;
+            this.vScrollBar3.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar3_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 317);
+            this.Controls.Add(this.vScrollBar3);
+            this.Controls.Add(this.vScrollBar2);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt_1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.VScrollBar vScrollBar2;
+        private System.Windows.Forms.VScrollBar vScrollBar3;
+    }
+}
+
---
+++ 2-DS-DESENVOLVIMENTO DE SISTEMAS/Aula 1/Exemplo2/exemplo2/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exemplo2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void vScrollBar3_Scroll(object sender, ScrollEventArgs e)
+        {
+            txt3.Text = vScrollBar3.Value.ToString();
+            BackColor = Color.FromArgb(vScrollBar3.Value, vScrollBar2.Value, vScrollBar1.Value);
+        }
+
+        private void vScrollBar2_Scroll(object sender, ScrollEventArgs e)
+        {
+            txt2.Text = vScrollBar2.Value.ToString();
+            BackColor = Color.FromArgb(vScrollBar3.Value, vScrollBar2.Value, vScrollBar1.Value);
+
+        }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            txt_1.Text = vScrollBar1.Value.ToString();
+            BackColor = Color.FromArgb(vScrollBar3.Value, vScrollBar2.Value, vScrollBar1.Value);
+        }
+    }
+}
